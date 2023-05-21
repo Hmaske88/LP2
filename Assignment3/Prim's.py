3ea4f2e68bf28486d47f737f11b7ce435b4c3534
Hemant Maske <89325376+Hmaske88@users.noreply.github.com>
2023-05-21 22:30:53 +0530

    Update Prim's.py

--- Assignment3/Prim's.py
+++ Assignment3/Prim's.py
@@ -27,4 +27,16 @@ g = {
     'E': {'B': 42, 'C': 66, 'D': 31}
 }
 
+# For user input Graph
+# graph={}
+# nodes=list(map(str,input("Enter the nodes in the graph separated by space :").split(" ")))
+# edgwt=[[x,y] for x,y in zip(map(str,input("Enter the edges separated by space : ").split(" ")), map(int,input("Enter their respective weights separated by space : ").split(" ")))]
+# for i in nodes:
+#     graph[i]={}
+#     for j in edgwt:
+#         if(j[0][0]==i):
+#             graph[i][j[0][1]]=j[1]
+#         if(j[0][1]==i):
+#             graph[i][j[0][0]]=j[1]
+
 print(prim(g,'A'))
