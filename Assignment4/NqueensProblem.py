883cc762207338321db43de5dfc94015db36020f
Hemant Maske <89325376+Hmaske88@users.noreply.github.com>
2023-05-21 19:00:05 +0530

    Update NqueensProblem.py

--- Assignment4/NqueensProblem.py
+++ Assignment4/NqueensProblem.py
@@ -1,48 +1,45 @@
-global N
-N = 4
-
-def printSolution(board):
-          for i in range(N):
-                    for j in range(N):
-                              print (board[i][j],end=' ')
-                    print()
-
-def isSafe(board, row, col):
-	for i in range(col):
-		if board[row][i] == 1:
-			return False
-
-	for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-		if board[i][j] == 1:
-			return False
-
-	for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-		if board[i][j] == 1:
-			return False
-	return True
-
-def solveNQUtil(board, col):
-	for i in range(N):
-		if isSafe(board, i, col):
-			board[i][col] = 1
-			if solveNQUtil(board, col + 1) == True:
-				return True
-			board[i][col] = 0
-	if col >= N:
-		return True
-	return False
+n=int(input("Enter the number of queens :"))
 
 def solveNQ():
-	board = [ [0, 0, 0, 0],
-		[0, 0, 0, 0],
-		[0, 0, 0, 0],
-		[0, 0, 0, 0]]
+    board=[[0]*n for i in range(n)]
+    
+    if solveNQUtil(board,0)==False:
+        print("Solution does not exist")
+        return False
+    else:
+        printSolution(board)
+        return True
 
-	if solveNQUtil(board, 0) == False:
-		print ("Solution does not exist")
-		return False
+def solveNQUtil(board, row):
+    if(row>=n):
+        return True
+    for j in range(n):
+        if isSafe(board,row,j):
+            board[row][j]=1
+            if(solveNQUtil(board,row+1)):
+                return True
+            board[row][j]=0
+    return False
 
-	printSolution(board)
-	return True
+def isSafe(board,row,col):
+    for a in range(n):              #for row
+        if(board[row][a]==1):
+            return False
+    for a in range(n):              #for column
+        if(board[a][col]==1):
+            return False
+    for a,b in zip(range(row,-1,-1),range(col,-1,-1)):   #for left diagonal
+        if(board[a][b]==1):
+            return False
+    for a,b in zip(range(row,-1,-1),range(col,n,1)):    #for right diagonal
+        if(board[a][b]==1):
+            return False
+    return True
+    
+def printSolution(board):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j],end=" ")
+        print()
 
 solveNQ()
